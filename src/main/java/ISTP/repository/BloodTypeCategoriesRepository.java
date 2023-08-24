c063e35b1c0db80c9d002b8d9e6f08eefb4042ea
Gyuhyeok <ghkdrbgur13@naver.com>
2023-08-24 17:51:06 +0900

    혈액형 테이블 추가

--- src/main/java/ISTP/repository/BloodTypeCategoriesRepository.java
+++ src/main/java/ISTP/repository/BloodTypeCategoriesRepository.java
@@ -1,7 +1,6 @@
 package ISTP.repository;
 
 import ISTP.domain.bloodDonation.BloodTypeCategories;
-import ISTP.domain.help.question.QuestionTypeCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BloodTypeCategoriesRepository extends JpaRepository<BloodTypeCategories, Long> {
