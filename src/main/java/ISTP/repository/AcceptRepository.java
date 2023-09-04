5751340ffe54874f796317b3b38b25505ecf0d76
chanhee <102280807+chanhee01@users.noreply.github.com>
2023-09-04 23:42:16 +0900

    [Update] RequestService, AcceptService 쿼리 최적화

--- src/main/java/ISTP/repository/AcceptRepository.java
+++ src/main/java/ISTP/repository/AcceptRepository.java
@@ -4,11 +4,13 @@ import ISTP.domain.bloodDonation.accept.Accept;
 import ISTP.domain.bloodDonation.accept.AcceptStatusCategories;
 import ISTP.domain.bloodDonation.request.RequestStatusName;
 import ISTP.domain.member.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AcceptRepository extends JpaRepository<Accept, Long> {
 
@@ -19,5 +21,9 @@ public interface AcceptRepository extends JpaRepository<Accept, Long> {
     @Query("select count(a) from Accept a join a.member m where m.id = :memberId and a.acceptStatusId = :statusId")
     Long count(@Param(value = "memberId") Long memberId, @Param(value = "statusId") Long status);
 
+    @EntityGraph(attributePaths = {"request"})
     List<Accept> findAllByAcceptStatusIdAndIsOngoing(Long acceptStatusId, Boolean ongoing);
+
+    @Query("select a from Accept a join fetch a.member m where a.id = :id")
+    Optional<Accept> findByIdWithMember(@Param(value = "id") Long acceptId);
 }
